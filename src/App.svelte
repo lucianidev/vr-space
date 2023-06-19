19b928fa11437f6cdadee65c3a83d21c2f8d77c7
= <=>
2023-06-19 03:32:53 +0200

    changes for testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
-<button class="btn">Hello daisyui</button>
+<script>
+    import NavBar from "./components/NavBar.svelte";
+    import Form from "./components/Form.svelte";
+    import Button from "./components/Button.svelte";
+
+    function j() {
+        console.log("llll");
+    }
+</script>
+
+<NavBar></NavBar>
+<Button insideText="heyy" action={j}></Button>
+<Form></Form>
