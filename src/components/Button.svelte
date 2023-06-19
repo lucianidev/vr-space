0e3295a914e65d16aaa9ce61ebd941c6cdd7bbe0
= <=>
2023-06-19 03:32:22 +0200

    created button and integrated 1-click function call, refactor needed

---
+++ src/components/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let insideText;
+    export let isContainigLink = false;
+    export let link = "";
+    export let action;
+    let isButtonClicked = false;
+    let currentVisibility;
+    const disable = () => {
+        const disabledClass = isButtonClicked ? "btn-disabled" : "";
+        isButtonClicked = true;
+        currentVisibility =  disabledClass;
+};
+</script>
+
+<button class="btn btn-primary {currentVisibility}" on:click={action} on:click={disable}>
+    {#if isContainigLink}
+        <a href={link}>{insideText}</a>
+    {:else}
+        {insideText}
+    {/if}
+</button>
