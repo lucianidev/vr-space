eb519f9cccdbee02f630c225e391ef933f514100
= <=>
2023-06-21 01:29:34 +0200

    changed teh action parameter to a normal call instead of a anonymus function and a call

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -5,14 +5,15 @@
     export let action;
     let isButtonClicked = false;
     let currentVisibility;
+
     const disable = () => {
-        const disabledClass = isButtonClicked ? "btn-disabled" : "";
+        const disabledClass = isButtonClicked ? "btn-disabled" : " ";
         isButtonClicked = true;
         currentVisibility =  disabledClass;
 };
 </script>
 
-<button class="btn btn-primary {currentVisibility}" on:click={action} on:click={disable}>
+<button class="btn btn-primary rounded-full {currentVisibility}" on:click={action()} on:click={disable}>
     {#if isContainigLink}
         <a href={link}>{insideText}</a>
     {:else}
