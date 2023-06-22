f07684a24dc755906952981b0f75c7c4509ffa37
= <=>
2023-06-23 01:28:55 +0200

    added icons

---
+++ src/components/posts/Icons.svelte
@@ -0,0 +1,9 @@
+<script>
+    // like calls database and do stuff 
+    // share copy to clipboard the id of the post
+    // for openig a shared link read it through the parameters of the link, then open it with in a single page. show content in a card
+</script>
+
+<ul>
+    <li></li>
+</ul>
