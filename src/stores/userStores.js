eb0c6e45e6fe98ab9a0d9b5891d675307257160f
= <=>
2023-06-22 16:53:56 +0200

    added auth functions to store

--- src/stores/userStores.js
+++ src/stores/userStores.js
@@ -1,11 +1,11 @@
-import { readable, writable } from "svelte/store";
-import { Account, Client } from "appwrite";
+import { writable } from "svelte/store";
+import { Account, Client, ID } from "appwrite";
 
 
 const createUserState = () => {
     const { set, update, subscribe } = writable({
         username: "",
-        isLogged: null,
+        isLogged: false,
         // TODO : implemnent loading state
     });
     const start = () => {
@@ -20,17 +20,13 @@ const createUserState = () => {
 
 
     return {
-        set,
-        update,
         subscribe,
 
-
-
         isLogged: async () => {
-            const account = start();
             try {
+                const account = start();
                 const userdata = await account.get();
-
+                console.log(userdata)
                 if (userdata) {
                     set({
                         username: userdata.name,
@@ -49,6 +45,59 @@ const createUserState = () => {
                 });
             }
         },
+
+        signup: async (email, password, username) => {
+            
+            try {
+                const account = start();
+                await account.create(ID.unique(), email, password, username);
+                set({
+                    username: username,
+                    isLogged: true,
+                });
+                console.log(username)
+                // router actions
+            } catch (error) {
+                set({
+                    username: "",
+                    isLogged: false,
+                });
+            }
+        },
+
+        login: async (email, password) => {
+            try {
+                const account = start();
+                await account.createEmailSession(email, password);
+                const userInfo = await account.get();
+                console.log(userInfo)
+                set({
+                    username: userInfo.name,
+                    isLogged: true,
+                });
+            } catch(error) {
+                set({
+                    username: "",
+                    isLogged: false,
+                });
+            }
+        },
+        logout: async (email, password) => {
+            try {
+                const account = start();
+                await account.account.deleteSessions();
+                set({
+                    username: "",
+                    isLogged: false,
+                });
+            } catch(error) {
+                set({
+                    username: "",
+                    isLogged: false,
+                });
+            }
+        },
+
     }
 }
 
