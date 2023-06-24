b64e3a77a0e58ba6ec1d864c8c916f9ceaf0b594
= <=>
2023-06-24 12:59:58 +0200

    implemented start function for postsStore

--- src/stores/postsStore.js
+++ src/stores/postsStore.js
@@ -1,22 +1,45 @@
 import { writable } from "svelte/store";
+import { Client, Databases, ID } from "appwrite";
 
 const createPostsStore = () => {
     const { set, update, subscribe } = writable({
         title : "",
         message : "",
         isOnFocus : false,
-        // TODO : implemnent image
+        // TODO : implemnent image 
     });
+
+    const start = () => {
+        const client = new Client();
+        const database = new Databases(client);
+
+        client
+        .setEndpoint("http://127.0.0.1:81/v1") // Your API Endpoint
+        .setProject("648f118e178c4607ca18"); // Your project ID
+
+        return database;
+    }
 
     return {
         subscribe,
-        setMessage : (title, message) => {
+        createPost : () => {
+
+        },
+        OnFocus : (title, message) => {
             set({
                 title : title,
                 message : message,
                 isOnFocus : true,
             });
-        }
+        },
+        
+        removeFocus : () => {
+            set({
+                title : "",
+                message : "",
+                isOnFocus : false,
+            });
+        },
     }
 }
 
