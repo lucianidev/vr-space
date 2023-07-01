2849325e9c374af97e68387d4e46227f030166f6
= <=>
2023-07-01 16:11:10 +0200

    added inside elements to form with slots

--- src/components/form/Form.svelte
+++ src/components/form/Form.svelte
@@ -5,7 +5,7 @@
 
 <form class="grid place-items-center w-full" on:submit|preventDefault={action}>
     <div
-      class="flex flex-col space-y-5 space-y-6 border-solid border-2 border-white w-5/12 rounded-t-xl bg-form p-7"
+      class="flex flex-col space-y-5 space-y-6 border-solid border-2 border-white w-full rounded-t-xl bg-form p-7 sm:w-5/12"
     >
       <h1 class="gradient-text text-2xl text-center">{formName}</h1>
 
