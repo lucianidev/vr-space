dd91ee02631684c747b1a6319f161b7edb987ca1
= <=>
2023-07-01 16:06:35 +0200

    converting images to ids when uploading products

---
+++ src/utils/convertImagesToId.js
@@ -0,0 +1,10 @@
+
+export const imagesToId = async (storage, images, id) => {
+    const ids = [];
+    for (const image of images) {
+        const convertedImage = (await storage.createFile("6499546407c2dc5f2d10", id, image)).$id;
+        ids.push(convertedImage);
+    }
+
+    return ids;
+};
