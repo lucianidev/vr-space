65c28ca23a64618e97f5b84ec74785e7fd59d796
= <=>
2023-07-01 16:10:18 +0200

    added type prop to button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -2,18 +2,19 @@
     export let insideText;
     export let isContainigLink = false;
     export let link = "";
+    export let type = "";
     export let action;
     let isButtonClicked = false;
     let currentVisibility;
 
     const disable = () => {
-        const disabledClass = isButtonClicked ? "btn-disabled" : " ";
+        const disabledClass = isButtonClicked ? "btn-disabled" : "";
         isButtonClicked = true;
         currentVisibility =  disabledClass;
 };
 </script>
 
-<button class="btn btn-primary rounded-full {currentVisibility}" on:click={action()} on:click={disable}>
+<button class="btn btn-primary rounded-full {currentVisibility}" on:click={disable} on:click={action} type={type}>
     {#if isContainigLink}
         <a href={link}>{insideText}</a>
     {:else}
