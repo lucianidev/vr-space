b11bc540170465f2f46057eb98f87870145fb6f4
= <=>
2023-07-01 16:15:41 +0200

    added like feature with prevention from multiple like

--- src/components/posts/Icons.svelte
+++ src/components/posts/Icons.svelte
@@ -1,9 +1,29 @@
 <script>
-    // like calls database and do stuff 
+    import { postsStore } from "../../stores/postsStore";
+
+
+    const likedPostUser = $postsStore.username;
+    const postdId = $postsStore.postId;
+    // implement like calls database and do stuff 
     // share copy to clipboard the id of the post
     // for openig a shared link read it through the parameters of the link, then open it with in a single page. show content in a card
+    // implement change of heart on click
 </script>
 
-<ul>
-    <li></li>
+<ul class="flex justify-between w-full">
+    <li class="w-8 h-8	 m-1">
+        <img class="" src="../../../assets/heart.svg" alt="" on:click={() => postsStore.ispostIsLiked(postdId).then(async isLiked => {
+            console.log(isLiked)
+            if(isLiked) return;
+            postsStore.likePost(postdId, likedPostUser);
+        })}>
+    </li>
+
+    <li class="	 m-1">
+        <img class="" src="../../../assets/send.svg" alt="">
+    </li>
+
+    <li class="	 m-1">
+        <img class="" src="../../../assets/report.svg" alt="">
+    </li>
 </ul>
