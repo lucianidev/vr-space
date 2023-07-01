0fae058436498c676f3d1e6bfd2ebb7fd2e352e1
= <=>
2023-07-01 16:19:02 +0200

    added avatar update and getter. need to refactor the init phase with deconstructuring ASAP

--- src/stores/userStores.js
+++ src/stores/userStores.js
@@ -1,21 +1,21 @@
 import { writable } from "svelte/store";
-import { Account, Client, Storage , ID } from "appwrite";
+import { Account, Client, Storage ,Databases,Query, ID } from "appwrite";
+import { postsStore } from "./postsStore";
 
 
 const createUserState = () => {
     const { set, update, subscribe } = writable({
         username: "",
         isLogged: false,
-        // TODO : implemnent loading state
     });
     const start = () => {
         const client = new Client();
-        const account = new Account(client);
+
 
         client
             .setEndpoint("http://127.0.0.1:81/v1") // Your API Endpoint
             .setProject("648f118e178c4607ca18"); // Your project ID
-        return account;
+        return client;
     }
 
 
@@ -24,7 +24,7 @@ const createUserState = () => {
 
         isLogged: async () => {
             try {
-                const account = start();
+                const account = new Account(start());
                 const userdata = await account.get();
 
                 if (userdata) {
@@ -49,14 +49,16 @@ const createUserState = () => {
         signup: async (email, password, username) => {
             
             try {
-                const account = start();
+                const account = new Account(start());
                 await account.create(ID.unique(), email, password, username);
+                account.updatePrefs({
+                    avatar_id : "",
+                });
                 set({
                     username: username,
                     isLogged: true,
                 });
                 console.log(username)
-                // router actions
             } catch (error) {
                 set({
                     username: "",
@@ -67,7 +69,7 @@ const createUserState = () => {
 
         login: async (email, password) => {
             try {
-                const account = start();
+                const account = new Account(start());
                 await account.createEmailSession(email, password);
                 const userInfo = await account.get();
                 console.log(userInfo)
@@ -82,9 +84,9 @@ const createUserState = () => {
                 });
             }
         },
-        logout: async (email, password) => {
+        logout: async () => {
             try {
-                const account = start();
+                const account = new Account(start());
                 await account.account.deleteSessions();
                 set({
                     username: "",
@@ -100,13 +102,81 @@ const createUserState = () => {
 
         getUserName : async() => {
             try {
-                const account = start();
+                const account = new Account(start());
                 const name = (await account.get()).name;
                 return await name;
             } catch(error) {
-                return "";
+                return;
             }
-        }
+        },
+
+        getAvatar : async() => {
+            try {
+                const client = start();
+                const account = new Account(client);
+
+                const avatarId = (await account.getPrefs()).avatar_id;
+
+                return avatarId;
+            } catch(error) {
+                return;
+            }
+        },
+
+        updateAvatar : async(file) => {
+            // i know this is bad, really bad but no time to refactor LMAO!!!!!:)
+            try {
+                const client = start();
+                const account = new Account(client);
+                const storage = new Storage(client);
+                const db = new Databases(client);
+
+                const currentAvatarId = (await account.getPrefs()).avatar_id;
+                
+                if(currentAvatarId) {
+                    storage.deleteFile('649aee3bd70a6aa2cb34',currentAvatarId);
+                    const avatarId = (await storage.createFile('649aee3bd70a6aa2cb34',ID.unique(),file)).$id;
+                    await account.updatePrefs({
+                        avatar_id : avatarId,
+                    });
+
+                    const posts = (await db.listDocuments('6492fa03477ec93ae650', '6492fa0b59b3b4f615fa', 
+                    [Query.equal('username', await userState.getUserName())])).documents;
+
+                    posts.forEach(post => {
+                        post.avatar_id = avatarId;
+                        db.updateDocument('6492fa03477ec93ae650', '6492fa0b59b3b4f615fa', post.$id, {
+                            title: post.title,
+                            description: post.description,
+                            username: post.username,
+                            date: post.date,
+                            image_id : post.image_id,
+                            avatar_id : avatarId,
+                        });
+                    })
+                } else {
+                    const avatarId = (await storage.createFile('649aee3bd70a6aa2cb34',ID.unique(),file)).$id;
+                    await account.updatePrefs({
+                        avatar_id : avatarId,
+                    });  
+                }
+
+            } catch(error) {
+                return;
+            }
+        },
+
+        deleteAvatar : async() => {
+            try {
+                const client = start();
+                const account = new Account(client);
+
+                const avatarId = (await account.getPrefs()).user_id;
+                storage.deleteFile('649aee3bd70a6aa2cb34', avatarId);
+            } catch(error) {
+                return;
+            }
+        },
     }
 }
 
