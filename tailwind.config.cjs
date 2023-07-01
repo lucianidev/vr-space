b6ac00d6f3dd3dc776b2663da7cd1b76e32c77d0
= <=>
2023-07-01 18:29:27 +0200

    updates #1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{svelte,js,ts}'],
   plugins: [require('daisyui')],
+
+  // daisyUI config (optional - here are the default values)
+
+
+  theme: {
+    extend: {
+      colors: {
+        'form': '#0b0b21',
+      },
+    }
+  }
 };
