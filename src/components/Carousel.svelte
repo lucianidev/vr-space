efdc3add3d0e646720f993bf535acffce2d1b16b
= <=>
2023-07-01 16:05:13 +0200

    adde carousel to product images

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { postsStore } from "../stores/postsStore";
+    export let images;
+</script>
+
+
+    <figure class="relative w-full">
+  
+      <!-- carousel -->
+      <div class="carousel w-full h-60 md:h-60 md:w-auto">
+        {#each images as imageId}
+        <div id={`item-${imageId}`} class="carousel-item w-full">
+            <img src={postsStore.preview('6499546407c2dc5f2d10', imageId)} class="w-full" />
+          </div> 
+        {/each}
+      </div>
+      <div class="flex justify-center w-full py-2 gap-2 absolute bottom-2 items-end">
+        {#each images as id}
+        <a href={`#item-${id}`} class="badge">•</a>
+        {/each}
+      </div>
+  
+    </figure>
+  
