5065d300833e24364ff8628b9f437833a1e84bff
= <=>
2023-07-01 16:15:00 +0200

    added prop to check if a post i product or social post

--- src/components/form/formVariations/UploadForm.svelte
+++ src/components/form/formVariations/UploadForm.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { Client, Databases, ID } from "appwrite";
   import Button from "../../Button.svelte";
   import FormInput from "../FormInput.svelte";
   import FormTextArea from "../FormTextArea.svelte";
@@ -9,12 +8,26 @@
 
   let title = "";
   let description = "";
+  let tags;
   let file = null;
+  let price = 0;
+  export let isProduct;
 </script>
-
+{#if isProduct}
 <Form formName="Post" action={() => postsStore.createPost(title, description, file)}>
+
   <FormFile bind:input={file} inputName="file" />
   <FormInput bind:input={title} inputName="Title" />
   <FormTextArea bind:input={description} inputName="Story" />
   <Button insideText="Upload" type="submit" />
 </Form>
+{:else}
+<Form formName="Post" action={() => postsStore.createProduct(title, description, file,tags,price)}>
+  <FormInput bind:input={title} inputName="Title" />
+  <FormFile bind:input={file} multiple={true} inputName="file"/>
+  <FormTextArea bind:input={description} inputName="description" />
+  <FormTextArea bind:input={tags} inputName="tags" />
+  <FormInput bind:input={price} inputName="Price" />
+  <Button insideText="Upload" type="Upload" />
+</Form>
+{/if}
