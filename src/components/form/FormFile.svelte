f0f56cb9e183f45c3ae6c28a25b32a565ae4a229
= <=>
2023-07-01 16:13:45 +0200

    added check if there is multiple files

--- src/components/form/FormFile.svelte
+++ src/components/form/FormFile.svelte
@@ -1,13 +1,23 @@
 <script>
     export let inputName = "";
     export let input;
+    export let multiple;
 </script>
 
-<div class="form-control w-full max-w-xs">
+<div class="form-control w-full max-w-xs" on:click>
+  {#if multiple}
+    <label for={inputName} class="label">
+      <span id={inputName} class="label-text text-white">{inputName}:</span>
+    </label>
+    <input type="file" bind:value={input} on:change={(e) => input = e.target.files}
+     class="file-input file-input-bordered file-input-primary w-full max-w-xs bg-black text-white"
+     multiple/>
+    {:else}
     <label for={inputName} class="label">
       <span id={inputName} class="label-text text-white">{inputName}:</span>
     </label>
     <input type="file" bind:value={input} on:change={(e) => input = e.target.files[0]}
      class="file-input file-input-bordered file-input-primary w-full max-w-xs bg-black text-white"
       />
+    {/if}
 </div>
