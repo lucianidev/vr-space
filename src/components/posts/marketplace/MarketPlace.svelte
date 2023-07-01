42abfa7359066b334b61d0f0eb7cb63035ed8c56
= <=>
2023-07-01 16:04:46 +0200

    added marketplace

---
+++ src/components/posts/marketplace/MarketPlace.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Product from "./Product.svelte";
+  import { postsStore } from "../../../stores/postsStore";
+  import { userState } from "../../../stores/userStores";
+  import { onMount } from "svelte";
+  let username = "";
+  onMount(async () => {
+    await userState.isLogged();
+    username = await userState.getUserName();
+  });
+
+  const products = postsStore.listProducts();
+</script>
+
+{#if $userState.isLogged}
+  <div
+    class=" flex flex-col lg:grid place-items-center w-full gap-4 grid-cols-3"
+  >
+    {#await products}
+      <p>loading</p>
+    {:then productsInfo}
+      {#each productsInfo.documents.filter((product) => product.username != username) as product}
+        <Product
+          title={product.title}
+          description={product.description}
+          username={product.username}
+          price={product.price}
+          images={product.images_id}
+          avatar={product.avatar}
+        />
+      {/each}
+    {/await}
+  </div>
+{:else}
+  <p>logjn please</p>
+{/if}
---
+++ src/components/posts/marketplace/Product.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { fade } from "svelte/transition";
+  import { postsStore } from "../../../stores/postsStore";
+  import { cutText } from "../../../utils/cutText";
+  import Button from "../../Button.svelte";
+  import Carousel from "../../Carousel.svelte";
+  import Avatar from "../../Avatar.svelte";
+
+  export let title;
+  export let description;
+  export let username;
+  export let price;
+  export let images;
+  export let avatar;
+
+
+</script>
+
+<div
+  transition:fade
+  on:click|stopPropagation
+  class="card bg-base-100 w-full shadow-xl"
+>
+  <div class="card-body">
+    <div class="avatar flex items-center justify-start">
+      <div class="w-10 rounded-full">
+        <Avatar avatarId={avatar} username={username}></Avatar>
+      </div>
+      <p class="m-2">{username}</p>
+    </div>
+    <Carousel images={images}></Carousel>
+    <h2 class="card-title">{title}</h2>  
+    <p>{cutText(description)}</p>
+    <p>{`${price}$`}</p>
+    <Button insideText="buy"></Button>
+  </div>
+</div>
