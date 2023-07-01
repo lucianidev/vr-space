b6fa63b64743d59eee86b2e004cb07d6c2c86927
= <=>
2023-07-01 16:07:19 +0200

    filtered the posts that are from the current account

---
+++ src/components/posts/Feed.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Message from "./messages/Message.svelte";
+  import { onMount } from "svelte";
+  import { userState } from "../../stores/userStores";
+  import MessageOnFocus from "./messages/MessageOnFocus.svelte";
+  import { postsStore } from "../../stores/postsStore";
+  import { setContext } from "svelte";
+  // implement show only if logged :: DONE
+  // implement text cutting : DONE
+  // make it responsive : DONE
+  // implement show focused post on click  :: DONE
+  let username = "";
+  onMount(async () => {
+    await userState.isLogged();
+    username = await userState.getUserName();
+  });
+
+  const posts = postsStore.readPosts();
+  postsStore.readPosts().then(data => console.log(data))
+</script>
+
+{#if $userState.isLogged}
+  <div
+    class=" flex flex-col lg:grid place-items-center w-full gap-4 grid-cols-3"
+  >
+    {#await posts}
+      <p>loading</p>
+    {:then postsInfo}
+      {#each postsInfo.documents.filter(post => post.username != username) as post}
+        <Message
+          on:click={() => {
+            postsStore.OnFocus(
+              post.title,
+              post.description,
+              post.username,
+              post.image_id,
+              post.avatar_id,
+              post.$id,
+            );
+          }}
+          username={post.username}
+          title={post.title}
+          message={post.description}
+          image={post.image_id}
+          avatar={post.avatar_id}
+        />
+      {/each}
+    {/await}
+  </div>
+  <MessageOnFocus />
+{:else}
+  <p>login my man, remeber router</p>
+{/if}
