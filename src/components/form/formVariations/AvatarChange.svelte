1a3c0bf0100d58a6a06cca9e2277075685a31254
= <=>
2023-07-03 09:59:00 +0200

    added form for avatar change

---
+++ src/components/form/formVariations/AvatarChange.svelte
@@ -0,0 +1,14 @@
+<script>
+      import Button from "../../Button.svelte";
+  import FormFile from "../FormFile.svelte";
+  import Form from "../Form.svelte";
+  import { userState } from "../../../stores/userStores";
+    
+
+  let file = null;
+</script>
+
+<Form formName="Avatar" action={() => userState.updateAvatar(file)}>
+    <FormFile bind:input={file} inputName="file" />
+    <Button insideText="Upload" type="submit" />
+  </Form>
