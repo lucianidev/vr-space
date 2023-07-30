dca7a1e189f18c1baaf254cfca71eaf1b75bfe73
= <=>
2023-07-31 00:18:44 +0300

    added new routes

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -18,10 +18,25 @@
 
   let pages = [
     {
-      title : 'links',
-      link : 'http://127.0.0.1/markeplace',
-      icon : '././assets/cart.svg',
-    }
+      title : 'Marketplace',
+      link : 'http://127.0.0.1:5173/marketplace',
+    },
+    {
+      title : 'Feed',
+      link : 'http://127.0.0.1:5173/',
+    },
+    {
+      title : 'create',
+      link : 'http://127.0.0.1:5173/create'
+    },
+    {
+      title : 'Settings',
+      link : 'http://127.0.0.1:5173/settings'
+    },
+    {
+      title : 'Profile',
+      link : 'http://127.0.0.1:5173/dashboard'
+    },
   ]
 
   // implements loop with links
@@ -29,9 +44,9 @@
 
 {#if $userState.isLogged}
 
-  <nav class="flex justify-end items-center bg-gray-50 dark:bg-gray-800">
+  <nav class="flex justify-end items-center bg-neutral-900 sticky z-50 top-0">
     <aside id="default-sidebar" class="{translateX} fixed top-16 left-0 z-40 w-64 h-screen transition-transform" on:click={() => interacttNav()} aria-label="Sidebar">
-      <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-900">
          <ul class="space-y-2 font-medium">
           {#each pages as page}
             <li>
@@ -45,14 +60,14 @@
       </div>
     </aside>
 
-    <div class="p-4">
+    <div class="flex items-start justify-between p-4">
       <Notifications></Notifications>
       <Avatar size={10} avatarId={$userState.avatarId} username={$userState.username}>
         <span class="m-1 text-xl">{$userState.username}</span>
       </Avatar>
 
 
-      <button data-drawer-target="default-sidebar" on:click={() => interacttNav()} data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+      <button data-drawer-target="default-sidebar" on:click={() => interacttNav()} data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center mt-2 ml-3 text-sm text-gray-500 rounded-lg ring-gray-200 dark:text-gray-400 ">
         <span class="sr-only">Open sidebar</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
