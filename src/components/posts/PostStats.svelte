3d5ad73ba3fb41687c5921542103a1be488a4676
= <=>
2023-08-16 20:44:19 +0200

    added link copying in user post

--- src/components/posts/PostStats.svelte
+++ src/components/posts/PostStats.svelte
@@ -1,5 +1,11 @@
 <script>
+    import { postsStore } from "../../stores/postsStore";
     export let likes;
+    export let id;
+    
+  function copyLinkToclipboard() {
+    navigator.clipboard.writeText(window.location.hostname +   '/share/' + id);
+  }
 </script>
 
 <ul class="flex justify-between items-end w-full">
@@ -10,7 +16,11 @@
         </span>
     </li>
 
-    <li class="	 m-1">
-        <img class="" src="../../../assets/send.svg" alt="">
-    </li>
+    <li class="	m-1">
+        <a href="#{$postsStore.postId}" class="">
+            <img class="" on:click={() => {
+                copyLinkToclipboard() ;
+                }} src="../../../assets/copy.svg" alt="" />
+        </a>
+      </li>
 </ul>
